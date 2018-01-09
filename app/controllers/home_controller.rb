5ba4c6be8a5a7bd363accc0a5ac412644148807f
Loi Tran <lvt15@my.fsu.edu>
2018-01-10 05:50:09 +0700

    Add rubocop and JWT gems and removed coinbase

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,25 +6,6 @@ class HomeController < ApplicationController
   BITFINEX = ['btcusd', 'ethusd', 'etcusd']
 
   def index
-    @markets = {coinbase: {}, bitfinex: {}}
-    COINBASE.each_with_index do |coin, index|
-      url = "https://api.coinbase.com/v2/prices/#{coin}/spot"
-      response = HTTParty.get(url)
-      response = response.parsed_response
-      price = response['data']['amount'].to_f
-      Price.create! price: price, currency: 'USD', market_id: 1, coin_id: index + 1
-      @markets[:coinbase][coin] = response
-    end
 
-    BITFINEX.each_with_index do |coin, index|
-      url = "https://api.bitfinex.com/v1/pubticker/#{coin}"
-      response = HTTParty.get(url)
-      response = response.parsed_response
-      price = response['last_price'].to_f
-      Price.create! price: price, currency: 'USD', market_id: 1, coin_id: index + 1
-      @markets[:bitfinex][coin[0..2].upcase] = response
-    end
-    @markets
   end
-
 end
