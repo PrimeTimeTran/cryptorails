bf838e96e147a85d204aefef184a53d9fb1e724f
Loi Tran <lvt15@my.fsu.edu>
2018-01-10 06:41:13 +0700

    Added authentication and add user model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
-require 'coinbase/wallet'
-require 'em-http'
-
 class HomeController < ApplicationController
   COINBASE = ['BTC-USD', 'ETH-USD']
   BITFINEX = ['btcusd', 'ethusd', 'etcusd']
 
   def index
-
+    @markets = Market.all
+    respond_to do |format|
+      format.json { render json: @markets }
+      format.html
+    end
   end
 end
