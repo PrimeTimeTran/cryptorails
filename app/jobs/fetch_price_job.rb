8007fc7cb519b54c4d2b08fc6aa7f7c34a3b01d0
Loi Tran <lvt15@my.fsu.edu>
2018-01-15 11:56:06 +0700

    Corrected Redis to work on the job

--- app/jobs/fetch_price_job.rb
+++ app/jobs/fetch_price_job.rb
@@ -10,8 +10,11 @@ class FetchPriceJob
       url = "https://api.coinbase.com/v2/prices/#{coin}/spot"
       response = HTTParty.get(url)
       response = response.parsed_response
-      price = response['data']['amount'].to_f
-      Price.create! price: price, currency: 'USD', market_id: 1, coin_id: index + 1
+      # price = response['data']['amount'].to_f
+      # price = Price.create! price: price, currency: 'USD', market_id: 1, coin_id: index + 1
+      data = {}
+      data[:html] = response
+      ActionCable.server.broadcast "prices", data
     end
 
     # BITFINEX.each_with_index do |coin, index|
